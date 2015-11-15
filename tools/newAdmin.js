4ccf2a762bdd7e801b98d9625a7e02d11fbefbb4
LEO <120039015@qq.com>
2015-11-14 22:43:25 -0500

    change tools

--- tools/newAdmin.js
+++ tools/newAdmin.js
@@ -16,25 +16,23 @@ mongoose.connect('mongodb://localhost/nodeapp');
 // });
 
 
-// passUtil.saltAndHash('admin', function(hash){
-// 	console.log(hash);
-// 	// newData.pass = hash;
-// 	// newData.date = moment().format('MMMM Do YYYY, h:mm:ss a');
-// 	// accounts.insert(newData, {safe: true}, callback);
-// 	var newAdmin = new Admin({
-// 	    username: 'admin',
-// 	    password: hash,
-// 	});
-// 	// save the sample user
-// 	newAdmin.save(function(err) {
-// 	    if (err) throw err;
-// 	    console.log('User saved successfully');
-// 	});
-// });
+passUtil.saltAndHash('admin', function(hash){
+	console.log(hash);
+
+	var newAdmin = new Admin({
+	    username: 'admin',
+	    password: hash,
+	});
+	// save the sample user
+	newAdmin.save(function(err) {
+	    if (err) throw err;
+	    console.log('User saved successfully');
+	});
+});
 
 // passUtil.validatePassword('admin', 'MHOOLVZ0AYe3f06f5e6c4c1b411f5c458fff92db98', function(err, result) {
 // 	console.log(result);
 // });
 //console.log(passUtil.validatePassword('admin', 'MHOOLVZ0AYe3f06f5e6c4c1b411f5c458fff92db98'));
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
-console.log(moment(new Date()).format('MMMM Do YYYY, h:mm:ss a'));
+// console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+// console.log(moment(new Date()).format('MMMM Do YYYY, h:mm:ss a'));
