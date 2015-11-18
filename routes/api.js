d45d7d796ede96edc00a5d4fd93154f1e4318e73
LEO <120039015@qq.com>
2015-11-17 19:47:17 -0500

    fixed typo

--- routes/api.js
+++ routes/api.js
@@ -149,7 +149,7 @@ authRouter.use(function(req, res, next) {
     } else {
     	return res.status(403).send({ 
     	    success: false, 
-    	    message: 'No token provided.' 
+    	    message: 'Error' 
     	});
     }
 });
